2e152f23dd60cbca49aa4d11aca762e969e26bc1
Damian Recoskie <damian_programmer@hotmail.com>
2014-12-13 17:21:23 -0500

    no multithreaded player animation and movement.

--- engine/player.py
+++ engine/player.py
@@ -1,13 +1,7 @@
 import sfml as sf
 import pathfinding
-from threading import Thread
-
 
 class Player:
-
-    #these variables are used to manage the players animatio and when to move
-    PlayerLoaded = False
-    PlayerMove = False
 
     def __init__(self, mapObject, window):
         self.window = window
@@ -15,14 +9,6 @@ class Player:
         self.pathfinder = None
         self.lightShader = sf.Shader.from_file(fragment="resources/shaders/light.frag")
         self.lightShader.set_parameter("lcolor", sf.Color(255,255,255, 50))
-
-        #start the players animation thread and keep it runing to animate and move player with delayes sepratly
-        #reaon to do it this way is that phython starts threads slowly
-
-        t = Thread(target=self.MoveThread, args=( window, ) )
-        t.start()
-
-        #set up player Dictonayiy values
 
         self.properties = dict(
             name = "",
@@ -82,7 +68,7 @@ class Player:
         self.physics = dict(
             x = 0,
             y = 0,
-            speed = 5,
+            speed = 1.6,
             direction = 0
         )
 
@@ -91,6 +77,8 @@ class Player:
             y = int
         )
         
+
+    clock = sf.system.Clock()
     cooldown = 100
     #last = sdl2.timer.SDL_GetTicks()
 
@@ -119,94 +107,68 @@ class Player:
         self.physics['speed'] = speed
         self.cooldown = 100 / speed
 
-    #multy player threaded Animation and movment
+    def moveRight(self, window):
+        self.oldpos = self.physics['x'], self.physics['y']
+        self.physics['x'] += self.physics['speed']
+        self.physics['direction'] = 3
+        self.drawPlayer(window, self.animations['animRight'])
 
-    #Move Up is Direction 0
-    #Move Left is Diection 1
-    #Move Down is Direction 2
-    #Move Right is Direction 3
+        if self.clock.elapsed_time.milliseconds >= self.cooldown:
+            self.clock.restart()
+            self.animations['animRight'] += 1
+            if self.animations['animRight'] == 9:
+               self.animations['animRight'] = 0
+            #pygame.time.wait(60)
 
-    def MoveThread(self, window ):
+    def moveLeft(self, window):
+        self.oldpos = self.physics['x'], self.physics['y']
+        self.physics['x'] -= self.physics['speed']
+        self.physics['direction'] = 1
+        self.drawPlayer(window, self.animations['animLeft'])
 
-        print "Thread Start"
-
-        #clock = sf.Clock() sucks that clock has A lower performance
-
-        self.PlayerLoaded = True
-
-        while self.PlayerLoaded:
-
-            if self.PlayerMove:
-
-                #if clock.elapsed_time.milliseconds >= self.cooldown: old way
-
-                    #clock.restart() old way
-
-                sf.sleep(sf.milliseconds(self.cooldown)) #as player is moveing dellay amination thread to the rate of cool down
-
-                if self.physics['direction'] == 0:
-                    self.oldpos = self.physics['x'], self.physics['y']
-                    self.physics['y'] -= self.physics['speed']
-                    self.animations['animUp'] += 1
-                    if self.animations['animUp'] == 9:
-                        self.animations['animUp'] = 0
-
-                if self.physics['direction'] == 1:
-                    self.oldpos = self.physics['x'], self.physics['y']
-                    self.physics['x'] -= self.physics['speed']
-                    self.animations['animLeft'] += 1
-                    if self.animations['animLeft'] == 9:
-                        self.animations['animLeft'] = 0
-
-                if self.physics['direction'] == 2:
-                    self.oldpos = self.physics['x'], self.physics['y']
-                    self.physics['y'] += self.physics['speed']
-                    self.animations['animDown'] += 1
-                    if self.animations['animDown'] == 9:
-                        self.animations['animDown'] = 0
-
-                if self.physics['direction'] == 3:
-                    self.oldpos = self.physics['x'], self.physics['y']
-                    self.physics['x'] += self.physics['speed']
-                    self.animations['animRight'] += 1
-                    if self.animations['animRight'] == 9:
-                        self.animations['animRight'] = 0
-
-            #player is not moving
-
-            else:
-
-                self.animations['animUp'] = 0
+        if self.clock.elapsed_time.milliseconds >= self.cooldown:
+            self.clock.restart()
+            self.animations['animLeft'] += 1
+            if self.animations['animLeft'] == 9:
                 self.animations['animLeft'] = 0
+            #pygame.time.wait(60)
+        
+    def moveUp(self, window):
+        self.oldpos = self.physics['x'], self.physics['y']
+        self.physics['y'] -= self.physics['speed']
+        self.physics['direction'] = 0
+        self.drawPlayer(window, self.animations['animUp'])
+
+        if self.clock.elapsed_time.milliseconds >= self.cooldown:
+            self.clock.restart()
+            self.animations['animUp'] += 1
+            if self.animations['animUp'] == 9:
+                self.animations['animUp'] = 0
+            #pygame.time.wait(60)
+
+    def moveDown(self, window):
+        self.oldpos = self.physics['x'], self.physics['y']
+        self.physics['y'] += self.physics['speed']
+        self.physics['direction'] = 2
+        self.drawPlayer(window, self.animations['animDown'])
+
+        if self.clock.elapsed_time.milliseconds >= self.cooldown:
+            self.clock.restart()
+            self.animations['animDown'] += 1
+            if self.animations['animDown'] == 9:
                 self.animations['animDown'] = 0
-                self.animations['animRight'] = 0
+            #pygame.time.wait(60)
 
-                sf.sleep( sf.milliseconds( 1000 / 60 ) ) #update at 60 times A second this stops the player thread amimation from killing cpu
-
-    def drawPlayer(self, window ):
-
-        #find sprite X
-
-        spriteX = 0
-
-        if self.physics['direction'] == 0:
-            spriteX = self.animations['animUp']
-        if self.physics['direction'] == 1:
-            spriteX = self.animations['animLeft']
-        if self.physics['direction'] == 2:
-            spriteX = self.animations['animDown']
-        if self.physics['direction'] == 3:
-            spriteX = self.animations['animRight']
-
+    def drawPlayer(self, window, spriteX):
         if self.isColliding(self.physics['x'], self.physics['y'], self.currentMap.tiles['blocked']):
             if self.pathfinder.variables['pathEnd']:
                 self.physics['x'], self.physics['y'] = self.oldpos['x'], self.oldpos['y']
 
-        #self.lightShader.set_parameter("light", (self.physics['x'], self.physics['y']))
+        self.lightShader.set_parameter("light", (self.physics['x'], self.physics['y']))
 
-        #renderS = sf.RenderStates()
-        #renderS.blend_mode = sf.BlendMode.BLEND_ADD
-        #renderS.shader = self.lightShader
+        renderS = sf.RenderStates()
+        renderS.blend_mode = sf.BlendMode.BLEND_ADD
+        renderS.shader = self.lightShader
         
         self.sprites['playerBody'].tileData['tiles'][spriteX][self.physics['direction']].position = (self.physics['x'], self.physics['y'])
         self.sprites['playerFeet'].tileData['tiles'][spriteX][self.physics['direction']].position = (self.physics['x'], self.physics['y'])
@@ -218,8 +180,7 @@ class Player:
         self.window.draw(self.sprites['playerFeet'].tileData['tiles'][spriteX][self.physics['direction']])
         self.window.draw(self.sprites['playerLegs'].tileData['tiles'][spriteX][self.physics['direction']])
         self.window.draw(self.sprites['playerChest'].tileData['tiles'][spriteX][self.physics['direction']])
-        self.window.draw(self.sprites['playerHead'].tileData['tiles'][spriteX][self.physics['direction']])
-        
+        self.window.draw(self.sprites['playerHead'].tileData['tiles'][spriteX][self.physics['direction']]) #, renderS)
         #window.blit(ps_copy, (self.physics['x'], self.physics['y']))
 
         #textFont = pygame.font.Font(None, 20)
